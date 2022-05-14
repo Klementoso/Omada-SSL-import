442a47c091f1fd51490cbb5f3d900b6ea67e8bab
Klementoso <105547320+Klementoso@users.noreply.github.com>
2022-05-14 14:25:47 +0200

    Compatibility with Omada 5.3.1
    Compatibility with Omada 5.3.1 (moved location of keystore)

--- omada_ssl_import.sh
+++ omada_ssl_import.sh
@@ -5,16 +5,16 @@
 # Tested on Ubuntu 20.04
 # This script is a modified version of the script made by user Ermax on TP-Link forums
 # See https://community.tp-link.com/en/business/forum/topic/96192?page=2 for the original post and script
-# Version 1.0
-# Last Updated Mar 17 2022
+# Version 1.1
+# Last Updated May 14 2022
 
 OMADA_HOSTNAME=mycontrollerurl.domain
 OMADA_SERVICE=omada
 
 service "${OMADA_SERVICE}" stop
-rm /opt/tplink/EAPController/keystore/eap.cer
-rm /opt/tplink/EAPController/keystore/eap.keystore
-cp /root/.acme.sh/"${OMADA_HOSTNAME}"/"${OMADA_HOSTNAME}".cer /opt/tplink/EAPController/keystore/eap.cer
+rm /opt/tplink/EAPController/data/keystore/eap.cer
+rm /opt/tplink/EAPController/data/keystore/eap.keystore
+cp /root/.acme.sh/"${OMADA_HOSTNAME}"/"${OMADA_HOSTNAME}".cer /opt/tplink/EAPController/data/keystore/eap.cer
 openssl pkcs12 -export -in /root/.acme.sh/"${OMADA_HOSTNAME}"/"${OMADA_HOSTNAME}".cer -inkey /root/.acme.sh/"${OMADA_HOSTNAME}"/"${OMADA_HOSTNAME}".key -name eap -out /root/.acme.sh/"${OMADA_HOSTNAME}"/"${OMADA_HOSTNAME}".p12 -password pass:tplink
-keytool -importkeystore -deststorepass tplink -destkeystore /opt/tplink/EAPController/keystore/eap.keystore -srckeystore /root/.acme.sh/"${OMADA_HOSTNAME}"/"${OMADA_HOSTNAME}".p12 -srcstoretype PKCS12 -srcstorepass tplink
+keytool -importkeystore -deststorepass tplink -destkeystore /opt/tplink/EAPController/data/keystore/eap.keystore -srckeystore /root/.acme.sh/"${OMADA_HOSTNAME}"/"${OMADA_HOSTNAME}".p12 -srcstoretype PKCS12 -srcstorepass tplink
 service "${OMADA_SERVICE}" start
